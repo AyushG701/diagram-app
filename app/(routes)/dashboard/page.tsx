5f6758b4bce9067a384cce9759b5c36af14e5329
Aayush701 <ayushghimire70111@gmail.com>
2024-07-05 15:30:40 +0545

    used the alternate query (one offquery) used when we know that parameter value is coming but will take time

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -6,29 +6,34 @@ import {
   useKindeBrowserClient,
 } from "@kinde-oss/kinde-auth-nextjs";
 import React, { useEffect } from "react";
-import { useMutation, useQuery } from "convex/react";
+import { useConvex, useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 
 function Dashboard() {
   const { user }: any = useKindeBrowserClient();
   const getUser = useQuery(api.user.getUser, { email: user?.email });
   const createUser = useMutation(api.user.createUser);
-
+  const convex = useConvex();
   useEffect(() => {
     if (user) {
       console.log(getUser);
-      if (getUser == undefined) {
-        createUser({
-          name: user?.given_name,
-          email: user?.email,
-          image: user.picture,
-        }).then((resp) => {
-          console.log(resp);
-        });
-      }
+      checkUser();
     }
     console.log("Dashboard");
   }, [user]);
+
+  const checkUser = async () => {
+    const result = await convex.query(api.user.getUser, { email: user?.email });
+    if (!result?.length) {
+      createUser({
+        name: user?.given_name,
+        email: user?.email,
+        image: user.picture,
+      }).then((resp) => {
+        console.log(resp);
+      });
+    }
+  };
   return (
     <div>
       Dashboard
